265750a9f1015cfda9d275578abd5f7f5b8e7262
Gabriel <gabrielverissimo735@gmail.com>
2025-11-21 20:09:16 -0300

    apenas atualizando o pacote model (mudando nome)

--- src/main/java/br/univ/pharmasys/model/Medicamento.java
+++ src/main/java/br/univ/pharmasys/model/Medicamento.java
@@ -1,14 +1,6 @@
-package model;
+package br.univ.pharmasys.model;
 
-    /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author gabri
- */
 public class Medicamento {
     
     private String sku;
