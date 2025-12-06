393ef4cb852a29adf7fb3befa68a5f5701f04bb9
Ricardo Fragoso <ricardofsilva0412@gmail.com>
2025-12-06 20:27:38 -0300

    feat: criação do DAO de cliente e implementação do create e busca por cpf

---
+++ src/main/java/br/univ/pharmasys/dao/ClienteDAO.java
@@ -0,0 +1,55 @@
+package br.univ.pharmasys.dao;
+
+import br.univ.pharmasys.model.Cliente;
+import br.univ.pharmasys.util.ConnectionFactory; // Ajuste o pacote se necessário
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import java.sql.Connection;
+
+public class ClienteDAO {
+
+    public void create(Cliente cliente){
+        String sql = "INSERT INTO CLIENTE (CPF_CLIENTE, NOME, SEXO) VALUES (?, ?, ?)";
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, cliente.getCpf());
+            stmt.setString(2, cliente.getNome());
+            stmt.setString(3, cliente.getSexo());
+
+            stmt.execute();
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao cadastrar Cliente: " + e.getMessage(), e);
+        }
+    }
+
+    public Cliente buscarPorCpf(String cpf){
+        String sql = "SELECT * FROM CLIENTE WHERE CPF_CLIENTE = ?";
+        Cliente cliente = null;
+
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, cpf);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    cliente = new Cliente();
+                    cliente.setCpf(rs.getString("CPF_CLIENTE"));
+                    cliente.setNome(rs.getString("NOME"));
+                    cliente.setSexo(rs.getString("SEXO"));
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao buscar Cliente por CPF: " + e.getMessage(), e);
+        }
+        return cliente;
+    }
+}
