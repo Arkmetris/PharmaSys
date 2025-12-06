0c100e9b006526887ae7d62d06d54d12b15bea1a
LuanVictor <lvssdeveloper@gmail.com>
2025-12-06 14:29:29 -0300

    Validador

---
+++ src/main/java/br/univ/pharmasys/service/ValidadorUtils.java
@@ -0,0 +1,56 @@
+
+	import java.util.regex.Pattern;
+	import java.util.InputMismatchException;
+
+	public class ValidadorUtils {
+
+	    public static boolean isEmailValido(String email) {
+	        if (email == null || email.isEmpty()) return false;
+	        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+	        Pattern pat = Pattern.compile(emailRegex);
+	        return pat.matcher(email).matches();
+	    }
+	    
+	    public static boolean isCpfValido(String cpf) {
+	    	
+	        cpf = cpf.replaceAll("[^0-9]", "");
+
+	        if (cpf.equals("00000000000") || cpf.equals("11111111111") ||
+	            cpf.length() != 11) return false;
+
+	        char dig10, dig11;
+	        int sm, i, r, num, peso;
+
+	        try {
+	            sm = 0;
+	            peso = 10;
+	            for (i = 0; i < 9; i++) {
+	                num = (int) (cpf.charAt(i) - 48);
+	                sm = sm + (num * peso);
+	                peso = peso - 1;
+	            }
+
+	            r = 11 - (sm % 11);
+	            if ((r == 10) || (r == 11)) dig10 = '0';
+	            else dig10 = (char) (r + 48);
+	            
+	            sm = 0;
+	            peso = 11;
+	            for (i = 0; i < 10; i++) {
+	                num = (int) (cpf.charAt(i) - 48);
+	                sm = sm + (num * peso);
+	                peso = peso - 1;
+	            }
+
+	            r = 11 - (sm % 11);
+	            if ((r == 10) || (r == 11)) dig11 = '0';
+	            else dig11 = (char) (r + 48);
+
+	            return (dig10 == cpf.charAt(9)) && (dig11 == cpf.charAt(10));
+	            
+	        } catch (InputMismatchException erro) {
+	            return false;
+	        }
+	    }
+	    
+	}
