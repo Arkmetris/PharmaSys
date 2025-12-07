17c3440a6a17ac967419087f53485dc034797207
LuanVictor <lvssdeveloper@gmail.com>
2025-12-07 13:02:21 -0300

    Correções e remoção do InputMismatchException

--- src/main/java/br/univ/pharmasys/service/ValidadorUtils.java
+++ src/main/java/br/univ/pharmasys/service/ValidadorUtils.java
@@ -12,8 +12,8 @@
 	    }
 	    
 	    public static boolean cpfValido(String cpf) {
-	    	
-	    	if (cpf == null) return false;
+
+	        if (cpf == null) return false;
 
 	        cpf = cpf.replaceAll("\\D", "");
 
@@ -24,81 +24,74 @@
 	        char dig10, dig11;
 	        int sm, i, r, num, peso;
 
-	        try {
-	            sm = 0;
-	            peso = 10;
-	            for (i = 0; i < 9; i++) {
-	                num = (int) (cpf.charAt(i) - 48);
-	                sm = sm + (num * peso);
-	                peso = peso - 1;
-	            }
-
-	            r = 11 - (sm % 11);
-	            if ((r == 10) || (r == 11)) dig10 = '0';
-	            else dig10 = (char) (r + 48);
-	            
-	            sm = 0;
-	            peso = 11;
-	            for (i = 0; i < 10; i++) {
-	                num = (int) (cpf.charAt(i) - 48);
-	                sm = sm + (num * peso);
-	                peso = peso - 1;
-	            }
-
-	            r = 11 - (sm % 11);
-	            if ((r == 10) || (r == 11)) dig11 = '0';
-	            else dig11 = (char) (r + 48);
-
-	            return (dig10 == cpf.charAt(9)) && (dig11 == cpf.charAt(10));
-	            
-	        } catch (InputMismatchException erro) {
-	            return false;
+	        sm = 0;
+	        peso = 10;
+	        for (i = 0; i < 9; i++) {
+	            num = (int) (cpf.charAt(i) - 48);
+	            sm = sm + (num * peso);
+	            peso = peso - 1;
 	        }
+
+	        r = 11 - (sm % 11);
+	        if ((r == 10) || (r == 11)) dig10 = '0';
+	        else dig10 = (char) (r + 48);
+	        
+	        sm = 0;
+	        peso = 11;
+	        for (i = 0; i < 10; i++) {
+	            num = (int) (cpf.charAt(i) - 48);
+	            sm = sm + (num * peso);
+	            peso = peso - 1;
+	        }
+
+	        r = 11 - (sm % 11);
+	        if ((r == 10) || (r == 11)) dig11 = '0';
+	        else dig11 = (char) (r + 48);
+	        return (dig10 == cpf.charAt(9)) && (dig11 == cpf.charAt(10));
 	    }
 	    
 	    public static boolean cnpjValido(String cnpj) {
-	    	
-	    	if (cnpj == null) return false;
+
+	        if (cnpj == null) return false;
+
+
 	        cnpj = cnpj.replaceAll("\\D", "");
-	        
+
+
 	        if (cnpj.length() != 14 || cnpj.matches("(\\d)\\1{13}")) return false;
+
 
 	        char dig13, dig14;
 	        int sm, i, r, num, peso;
 
-	        try {
-	           
-	            sm = 0;
-	            peso = 2;
-	            for (i = 11; i >= 0; i--) {
-	                num = (int) (cnpj.charAt(i) - 48);
-	                sm = sm + (num * peso);
-	                peso = peso + 1;
-	                if (peso == 10) peso = 2;
-	            }
 
-	            r = sm % 11;
-	            if ((r == 0) || (r == 1)) dig13 = '0';
-	            else dig13 = (char) ((11 - r) + 48);
-
-	            
-	            sm = 0;
-	            peso = 2;
-	            for (i = 12; i >= 0; i--) {
-	                num = (int) (cnpj.charAt(i) - 48);
-	                sm = sm + (num * peso);
-	                peso = peso + 1;
-	                if (peso == 10) peso = 2;
-	            }
-
-	            r = sm % 11;
-	            if ((r == 0) || (r == 1)) dig14 = '0';
-	            else dig14 = (char) ((11 - r) + 48);
-
-	            return (dig13 == cnpj.charAt(12)) && (dig14 == cnpj.charAt(13));
-	        } catch (InputMismatchException erro) {
-	            return false;
+	        sm = 0;
+	        peso = 2;
+	        for (i = 11; i >= 0; i--) {
+	            num = (int) (cnpj.charAt(i) - 48);
+	            sm = sm + (num * peso);
+	            peso = peso + 1;
+	            if (peso == 10) peso = 2;
 	        }
+
+	        r = sm % 11;
+	        if ((r == 0) || (r == 1)) dig13 = '0';
+	        else dig13 = (char) ((11 - r) + 48);
+
+	        sm = 0;
+	        peso = 2;
+	        for (i = 12; i >= 0; i--) {
+	            num = (int) (cnpj.charAt(i) - 48);
+	            sm = sm + (num * peso);
+	            peso = peso + 1;
+	            if (peso == 10) peso = 2;
+	        }
+
+	        r = sm % 11;
+	        if ((r == 0) || (r == 1)) dig14 = '0';
+	        else dig14 = (char) ((11 - r) + 48);
+
+	        return (dig13 == cnpj.charAt(12)) && (dig14 == cnpj.charAt(13));
 	    }
 	    
 	}
