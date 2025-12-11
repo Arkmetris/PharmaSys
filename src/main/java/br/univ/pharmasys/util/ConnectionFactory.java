97c6f5abff2220be3f0514e96127308fd8f38342
Gabriel <gabrielverissimo735@gmail.com>
2025-12-11 20:18:45 -0300

    Recolocando Connection dnv

---
+++ src/main/java/br/univ/pharmasys/util/ConnectionFactory.java
@@ -0,0 +1,24 @@
+package br.univ.pharmasys.util;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    private static final String DATABASE_NAME = "PharmaSys";
+    private static final String URL = "jdbc:mysql://localhost:3306/" + DATABASE_NAME + "?useTimezone=true&serverTimezone=America/Recife";
+    private static final String USER = "root";
+    private static final String PASSWORD = "SUA_SENHA_AQUI"; // <--- Coloque a senha que você definiu no mysql
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+
+        } catch (SQLException e) {
+            System.err.println("FALHA AO CONECTAR AO BANCO DE DADOS!");
+            throw new RuntimeException("Erro de Conexão: " + e.getMessage(), e);
+        }
+    }
+}
