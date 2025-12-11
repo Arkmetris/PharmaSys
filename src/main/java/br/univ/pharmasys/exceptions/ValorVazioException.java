b3c79c65cb8285fafac5e10c42565c45c8963c85
klarisse <klarissemaria11@gmail.com>
2025-12-10 23:55:31 -0300

    ajustes no código

---
+++ src/main/java/br/univ/pharmasys/exceptions/ValorVazioException.java
@@ -0,0 +1,8 @@
+package br.univ.pharmasys.exceptions;
+
+public class ValorVazioException extends RuntimeException {
+    
+    public ValorVazioException(String msg) {
+        super(msg);
+    }
+}
--- src/main/java/br/univ/pharmasys/service/CodigoDeBarras.java
+++ src/main/java/br/univ/pharmasys/service/CodigoDeBarras.java
@@ -1,36 +1,50 @@
 package br.univ.pharmasys.service;
+
+import br.univ.pharmasys.exceptions.ValorVazioException;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.oned.EAN13Writer;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
+import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-///Geração do código de barras
+
 public class CodigoDeBarras {
    
-    String path = "C:\\Users\\CodigodeBarras";
-    
-    private String gerarCodigoBarrasEAN13(String valor) throws Exception {
-        if (valor == null || valor.trim().isEmpty()) {
-            throw new IllegalArgumentException("Valor do código de barras não pode estar vazio.");
-        }
+    private String gerarCodigoBarrasEAN13(String valor) throws RuntimeException{
+        String usuarioHome = System.getProperty("user.home");
+        System.out.println("user.home: " + usuarioHome);
+        
+        Path documentosPath = Paths.get(usuarioHome, "Documents");
+        System.out.println("Caminho para Documentos: " + documentosPath.toAbsolutePath());
 
+        if (valor == null || valor.trim().isEmpty()) {
+            throw new ValorVazioException("Valor do código de barras não pode estar vazio.");
+        } else if (valor.length() != 13) {
+            throw new ValorVazioException("Valor do código deve ter 13 digítos.");
+
+        }
         String nomeArquivo = valor.replaceAll("[^a-zA-Z0-9]", "_") + ".png";
-        Path caminhoCompleto = FileSystems.getDefault().getPath(path, nomeArquivo);
+        Path caminhoCompleto = FileSystems.getDefault().getPath(usuarioHome,nomeArquivo);
 
         EAN13Writer barcodeWriter = new EAN13Writer();
         BitMatrix bitMatrix = barcodeWriter.encode(valor, BarcodeFormat.EAN_13, 300, 100);
-        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", caminhoCompleto);
+        try {
+            MatrixToImageWriter.writeToPath(bitMatrix, "PNG", caminhoCompleto);
+        } catch (IOException ex) {
+            Logger.getLogger(CodigoDeBarras.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
         System.out.println("Código de barras EAN_13 gerado.: " + caminhoCompleto.toAbsolutePath());
         return caminhoCompleto.toAbsolutePath().toString();
     }
 
     public void criar(String skuMedicamento) throws Exception {
-        String CodigoDeBarrasGerado = gerarCodigoBarrasEAN13(skuMedicamento);
+        String codigoDeBarrasGerado = gerarCodigoBarrasEAN13(skuMedicamento);
         //TODO salvar codigo de barras no banco de dados: método esperado (skumedicamento,codigodebarrasgerado, tipodecodigoEan13)
     }
 }
-
-
