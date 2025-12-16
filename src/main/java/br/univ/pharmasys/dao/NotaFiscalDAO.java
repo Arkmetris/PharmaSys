6352d48b15fc65fd3cf2508093acef72f06d064f
LuanVictor <lvssdeveloper@gmail.com>
2025-12-16 19:22:45 -0300

    Criação da classe NotaFiscalDAO

---
+++ src/main/java/br/univ/pharmasys/dao/NotaFiscalDAO.java
@@ -0,0 +1,11 @@
+package br.univ.pharmasys.dao;
+
+import br.univ.pharmasys.model.NotaFiscal;
+import br.univ.pharmasys.util.ConnectionFactory;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotaFiscalDAO {
+}
