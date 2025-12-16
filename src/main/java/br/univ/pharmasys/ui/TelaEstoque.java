af87434c1e4ee35a7aa34948de543f78d00a3229
Mariana Clarice <mclaricesilva04@gmail.com>
2025-12-16 20:59:01 -0300

    style: tela estoquista padronizada como as outras telas iniciais

--- src/main/java/br/univ/pharmasys/ui/TelaEstoque.java
+++ src/main/java/br/univ/pharmasys/ui/TelaEstoque.java
@@ -99,13 +99,5 @@ public class TelaEstoque extends JFrame {
         });
     }
 
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            new TelaEstoque().setVisible(true);
-        });
-
-    }
-
     private JTable tabelaEstoque;
 }
